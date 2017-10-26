9cff1f5d062523bdef5b377343fff2e94e581d79
binsonChang <binson.chang@senao.com.tw>
2017-10-26 21:38:37 +0800

    homework2 finish

--- homework_1/ViewController.m
+++ homework_1/ViewController.m
@@ -25,27 +25,7 @@
     
 }
 
-//-(id) getJsonValue:(NSString*)resourceName withType:(NSString*)resourceType {
-//    NSError *error = nil;
-//    NSString *jsonPath = [[NSBundle mainBundle] pathForResource:resourceName ofType:resourceType];
-//    NSData *jsonData = [NSData dataWithContentsOfFile:jsonPath];
-//
-//    return [NSJSONSerialization JSONObjectWithData:jsonData options:kNilOptions error:&error];
-//}
-//
-//- (void) printConfig:(ConfigManager *)configManager {
-//    for (int i=0; i<configManager.configs.count; i++) {
-//        Config *configObj = [configManager.configs objectAtIndex:i];
-//        NSLog(@"\n===============================================\nconfig[%d]\next: %@\nlocation: %@\nsubDirectory: %d\nunit: %@\nremove: %d\nhandler: %@\ndestination: %@\ndir: %@\nconnectionString: %@\n===============================================\n",i,configObj.ext,configObj.location,configObj.subDirectory,configObj.unit,configObj.remove,configObj.handler,configObj.destination,configObj.dir,configObj.connectionString);
-//    }
-//}
-//
-//- (void) printSchedule:(ScheduleManager *)scheduleManager {
-//    for (int i=0; i<scheduleManager.scheduleArry.count; i++) {
-//        Schedule *scheduleObj = [scheduleManager.scheduleArry objectAtIndex:i];
-//        NSLog(@"\n===============================================\nschedule[%d]\next: %@\ntime: %@\ninerval: %@\n===============================================\n",i,scheduleObj.ext,scheduleObj.time,scheduleObj.interval);
-//    }
-//}
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
